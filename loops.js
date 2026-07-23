4763f7fcd7b48839cc0448be836927b7dc6f774f
{Your_Full_Name} <{your_email@example.com}>
2026-07-23 22:59:17 +0545

    Day4:loops

---
+++ loops.js
@@ -0,0 +1,16 @@
+for (i=1; i<=10; i++) {
+    console.log(i);
+}
+
+
+for (let i=1; i<=20; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+let count=10;
+while(count>=1){
+    console.log(count);
+    count--;
+}
