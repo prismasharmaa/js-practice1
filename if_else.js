3f15c0123c1132da9f161d69012b653ea5ad0917
{Your_Full_Name} <{your_email@example.com}>
2026-07-23 22:43:48 +0545

    Day3:if_else

---
+++ if_else.js
@@ -0,0 +1,21 @@
+let marks = 75;
+
+if (marks >= 90) {
+  console.log("Grade: A");
+} else if (marks >= 75) {
+  console.log("Grade: B");
+} else if (marks >= 50) {
+  console.log("Grade: C");
+} else {
+  console.log("Fail");
+}
+
+
+let age = 20;
+let hasLicense = true;
+
+if (age >= 18 && hasLicense) {
+  console.log("You can drive");
+} else {
+  console.log("You cannot drive");
+}
