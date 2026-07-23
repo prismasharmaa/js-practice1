c74936c143917b4974744df8c7208a2dbef09014
{Your_Full_Name} <{your_email@example.com}>
2026-07-23 22:35:09 +0545

    Day2:operators

---
+++ operators.js
@@ -0,0 +1,30 @@
+
+
+let a = 20;
+let b = 6;
+
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b);
+
+
+
+console.log("Is a greater than b?", a > b);
+
+
+let isRaining = true;
+let hasUmbrella = false;
+
+console.log("Raining AND has umbrella:", isRaining && hasUmbrella);
+console.log("Raining OR has umbrella:", isRaining || hasUmbrella);
+console.log("NOT raining:", !isRaining);
+
+let temp = 15;
+let isWindy = true;
+
+console.log(temp > 20 || isWindy);
+console.log(temp > 20 && isWindy);
+console.log(!isWindy);
