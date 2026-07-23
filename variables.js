f8cc574df4d25da2447ffd44e74eede5d0213efe
{Your_Full_Name} <{your_email@example.com}>
2026-07-23 22:18:38 +0545

    day1:variables and data types

---
+++ variables.js
@@ -0,0 +1,13 @@
+
+
+let name = "YourNameHere";
+const age = 22;
+let isEmployed = false;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Employed:", isEmployed);
+
+console.log("Type of name:", typeof name);
+console.log("Type of age:", typeof age);
+console.log("Type of isEmployed:", typeof isEmployed);
