59eb8f992caa3e02fd4f909737ceb1148d6a981d
{Your_Full_Name} <{your_email@example.com}>
2026-07-25 20:00:02 +0545

    Day 7:objects

---
+++ objects.js
@@ -0,0 +1,17 @@
+let personalinfo= {
+
+    name: 'Prisma',
+    age: 22,
+    isemployed: false,
+     skills: ["JS", "HTML", "CSS"]
+};
+console.log(personalinfo);
+
+personalinfo.learning='JAVASCRIPT';
+console.log(personalinfo);
+
+console.log("Looping through skills:");
+for (let i = 0; i < me.skills.length; i++) {
+  console.log(me.skills[i]);
+}
+
